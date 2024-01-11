95d488abd178d2a351317fa40f1320851b373a5d
Chris Kay <chris@eleet.com.au>
2024-01-11 17:26:24 +1100

    adding a new card

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.querySelector("#cards-el")
+let sumEl = document.querySelector("#sum-el")
 
-if ( sum <= 20 ) {
-    message = "Do you want to draw a new card? 🙂"
-} else if ( sum === 21 ) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackjack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function startGame () {
+    if ( sum <= 20 ) {
+        message = "Do you want to draw a new card?"
+    } else if ( sum === 21 ) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+    cardsEl.textContent = "Cards: " + firstCard + " & " + secondCard
+    sumEl.textContent = "Sum: " + sum
 }
 
-console.log(message)
+function newCard() {
+    console.log("New Card!")
+}
