c2fd8581e4a6df406d9ffd2e9f166e41101d0398
Chris Kay <chris@eleet.com.au>
2024-01-11 15:51:54 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let firstCard = 10
+let secondCard = 10
+let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
+let message
+
+if ( sum <= 20 ) {
+    message = "Do you want to draw a new card? 🙂"
+} else if ( sum === 21 ) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackjack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+console.log(message)
