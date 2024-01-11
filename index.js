70930ce300a31a03014675eaadf0ba48fe885995
Chris Kay <chris@eleet.com.au>
2024-01-11 19:42:57 +1100

    final changes

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-
-let firstCard = 10
-let secondCard = 10
-let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
-let message
-let messageEl = document.getElementById("message-el")
+let message = ""
+let sum = 0
+let total = ""
+let cards = []
+let messageEl = document.querySelector("#message-el")
 let cardsEl = document.querySelector("#cards-el")
 let sumEl = document.querySelector("#sum-el")
 
-function startGame () {
+function startGame() {
+    sum = 0
+    cards.length = 0;
+    cards.push(getRandomCard())
+    cards.push(getRandomCard())
+    renderGame()
+}
+
+function getRandomCard() {
+    let num = Math.floor(Math.random() * 13) + 1
+    if ( num === 1 ) {
+        return 11
+    } else if ( num > 11 ) {
+        return 10
+    } else { 
+        return num
+    }
+}
+
+function sumCards(item) {
+    sum += item
+}
+
+function renderGame () {
+    cards.forEach(sumCards)
     if ( sum <= 20 ) {
         message = "Do you want to draw a new card?"
     } else if ( sum === 21 ) {
@@ -20,10 +43,16 @@ function startGame () {
         isAlive = false
     }
     messageEl.textContent = message
-    cardsEl.textContent = "Cards: " + firstCard + " & " + secondCard
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i<cards.length; i++) {
+        cardsEl.textContent += " : " + cards[i]
+    }
+
     sumEl.textContent = "Sum: " + sum
 }
 
 function newCard() {
-    console.log("New Card!")
+    sum = 0
+    cards.push(getRandomCard())
+    renderGame()
 }
